02ff3359972979570aa62cba32a5ce1addfea2cb
kavonrtep <petr@umbr.cas.cz>
2023-02-27 13:24:42 +0100

    code refactorization

--- tc_utils.py
+++ tc_utils.py
@@ -1,7 +1,10 @@
 """ Utilities for the project. """
 import os
+import subprocess
 import tempfile
 from itertools import cycle
+from shutil import rmtree
+import networkx as nx
 
 
 class Gff3Feature:
@@ -272,41 +275,6 @@ def recalculate_gff3_coordinates(gff3_file, matching_table):
     return gff3_file_recalculated
 
 
-# recalculate gff3 back to original coordinates, use gff3_feature class
-def recalculate_gff3_back_to_original_coordinates(
-        gff3_file, matching_table, chunk_size, overlap
-        ):
-    """
-    Recalculate gff3 back to original coordinates, use gff3_feature class
-    :param chunk_size: int
-    :param gff3_file: str
-    :param matching_table:
-    :return:
-    gff3_file_recalculated
-    """
-    gff3_file_recalculated = tempfile.NamedTemporaryFile(delete=False).name
-    with open(gff3_file, 'r') as fh_in:
-        with open(gff3_file_recalculated, 'w') as fh_out:
-            for line in fh_in:
-                if line[0] == '#':
-                    fh_out.write(line)
-                else:
-                    feature = Gff3Feature(line)
-                    ori_header, ori_start, ori_end, real_chunk_size = \
-                        get_original_header_and_coordinates(
-                        feature.seqid, feature.start, feature.end, matching_table
-                        )
-                    # if feature is too close (less than 100 nt) to the end ends of
-                    # chunk,skip it
-                    if feature.start < 100 or feature.end > real_chunk_size - 100:
-                        continue
-                    feature.seqid = ori_header
-                    feature.start = ori_start
-                    feature.end = ori_end
-                    fh_out.write(str(feature))
-    return gff3_file_recalculated
-
-
 def write_temp_fasta_chunks(fasta_seq_size, fasta_file, chunk_size):
     """
     Write temporary fasta chunks
@@ -340,3 +308,420 @@ def write_temp_fasta_chunks(fasta_seq_size, fasta_file, chunk_size):
                     fout.write(line)
     os.remove(fasta_file)
     return temp_files_fasta
+
+
+# TideHunter related functions:
+def run_tidehunter(fasta_file, tidehunter_arguments):
+    """run tidehunter on fasta file
+    require TideHunter to be in PATH
+    version of TideHunter must be 1.4.3
+    return path to tidehunter output file
+    """
+    # verify tidehunter version
+    tidehunter_version = subprocess.check_output(
+        "TideHunter -v", shell=True
+        ).decode().strip()
+    assert tidehunter_version == "1.4.3", "TideHunter version 1.4.3 is required"
+
+    # run tidehunter
+    tmp_file = fasta_file + ".out"
+    tidehunter_cmd = (F"TideHunter -f 2 -o {tmp_file} {tidehunter_arguments}"
+                      F" {fasta_file}")
+
+    print("running TideHunter")
+    print(tidehunter_cmd)
+    subprocess.check_call(tidehunter_cmd, shell=True)
+    return tmp_file
+
+
+class TideHunterFeature:
+    """class to store features from tidehunter output
+    """
+
+    def __init__(self, line):
+        self.seq_name = line.split()[0]
+        self.repeat_ID = line.split()[1] + "_" + line.split()[0]
+        self.seq_length = int(line.split()[2])
+        self.start = int(line.split()[3])
+        self.end = int(line.split()[4])
+        self.cons_length = int(line.split()[5])
+        self.copy_numer = float(line.split()[6])
+        self.aver_match = float(line.split()[7])
+        self.full_length = int(line.split()[8])
+        self.sub_position = line.split()[9]
+        self.consensus = line.split()[10]
+
+    def recalculate_coordinates(self, matching_table):
+        """recalculate coordinates in the table using matching table
+        """
+        ori_name, ori_start, ori_end, _ = get_original_header_and_coordinates(
+            self.seq_name, self.start, self.end, matching_table
+            )
+        self.seq_name = ori_name
+        self.start = ori_start
+        self.end = ori_end
+
+    def __str__(self):
+        return "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
+            self.seq_name, self.repeat_ID, self.seq_length, self.start, self.end,
+            self.cons_length, self.copy_numer, self.aver_match, self.full_length,
+            self.sub_position, self.consensus
+            )
+
+    def gff3(self):
+        """return line in gff3 format
+        """
+        columns = [self.seq_name, "TideHunter", "repeat_region", self.start, self.end,
+                   self.aver_match, ".", "."]
+        attributes = {"ID": self.repeat_ID, "Consensus_sequence": self.consensus,
+                      "Consensus_length": self.cons_length,
+                      "Copy_number": self.copy_numer, "Average_match": self.aver_match}
+
+        # put attributes in the right order and format, some are floats or ints - must be
+        # converted to string
+        columns_str = "\t".join([str(i) for i in columns])
+        atr_str = ";".join(["{}={}".format(k, str(v)) for k, v in attributes.items()])
+        gff_line = columns_str + "\t" + atr_str
+        return gff_line
+
+
+# clustering related functions:
+def fasta_to_dict(fasta_file):
+    """
+    convert fasta file to dictionary
+    :param fasta_file: path to fasta file
+    :return: dictionary with fasta sequences
+    """
+    fasta_dict = {}
+    with open(fasta_file, 'r') as f:
+        for line in f:
+
+            if line.startswith(">"):
+                seq_name = line.split()[0].replace(">", "")
+                fasta_dict[seq_name] = [""]  # initialize list
+            else:
+                fasta_dict[seq_name].append(line.strip())
+    # concatenate list in dictionary
+    fasta_dict = {k: "".join(v) for k, v in fasta_dict.items()}
+    return fasta_dict
+
+
+def get_seq_from_fasta(fasta_dict, seq_id, start=None, end=None):
+    """
+    get sequence from fasta dictionary
+    :param fasta_dict: dictionary with fasta sequences
+    :param seq_id: sequence ID
+    :param start: start position
+    :param end: end position
+    :return: sequence
+    """
+    if start and end:
+        return fasta_dict[seq_id][start:end]
+    return fasta_dict[seq_id]
+
+
+def gff3_to_fasta(gff3_file, fasta_file):
+    """
+    extract fasta sequences from gff3 file
+    it is generator, returns one sequence at time and seq ID
+    :param gff3_file: path to gff3 file
+    :param fasta_file: path to fasta file
+    :return:
+    """
+
+    fasta_dict = fasta_to_dict(fasta_file)
+    with open(gff3_file, 'r') as f1:
+        for line in f1:
+            if line.startswith("#"):
+                continue
+            gff3_feature: Gff3Feature = Gff3Feature(line)
+            s = get_seq_from_fasta(
+                fasta_dict, gff3_feature.seqid, gff3_feature.start, gff3_feature.end
+                )
+            yield [gff3_feature.attributes_dict['ID'], s,
+                   gff3_feature.attributes_dict['Consensus_sequence']]
+
+
+def save_fasta_dict_to_file(fasta_dict, fasta_file):
+    """
+    save fasta dictionary to file
+    :param fasta_dict: dictionary with fasta sequences
+    :param fasta_file: path to fasta file
+    :return:
+    """
+    with open(fasta_file, 'w') as f:
+        for k, v in fasta_dict.items():
+            f.write(">{}\n{}\n".format(k, v))
+
+
+# run mmseqs2 on consensus sequences
+def find_cluster_by_mmseqs2(sequences):
+    """
+    run mmseqs2 on consensus sequences
+    :param sequences:
+    :return: clusters
+    """
+    tmp_dir = tempfile.mkdtemp()
+    input_fasta_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
+    save_fasta_dict_to_file(sequences, input_fasta_file.name)
+
+    cmd = (F'mmseqs easy-cluster {input_fasta_file.name} {input_fasta_file.name}.clu'
+           F' {tmp_dir} --cluster-mode 0 '
+           F'--mask 0  -s 1')
+
+    subprocess.check_call(cmd, shell=True)
+
+    # read clusters to dictionary
+    with open(F"{input_fasta_file.name}.clu_cluster.tsv", 'r') as f:
+        clusters = {}
+        for line in f:
+            cluster, seq_id = line.split()
+            clusters[seq_id] = cluster
+    # remove temporary files
+    os.remove(F"{input_fasta_file.name}.clu_all_seqs.fasta")
+    os.remove(F"{input_fasta_file.name}.clu_cluster.tsv")
+    os.remove(F"{input_fasta_file.name}.clu_rep_seq.fasta")
+    rmtree(tmp_dir)
+    os.remove(input_fasta_file.name)
+    return clusters
+
+
+def make_graph(pairs):
+    """
+    make graph from list of pairs
+    :param pairs:
+    :return:
+    """
+    graph = {}
+    for pair in pairs:
+        v1, v2 = pair
+        if v1 not in graph:
+            graph[v1] = set()
+        if v2 not in graph:
+            graph[v2] = set()
+        graph[v1].add(v2)
+        graph[v2].add(v1)
+    return graph
+
+
+def get_connected_component_clusters(pairs):
+    """
+    find connected components in graph
+    :param pairs: list of pairs
+    :return: clusters as list of lists
+    """
+    # Create a NetworkX graph from the list of edges
+    g = nx.Graph()
+    g.add_edges_from(pairs)
+
+    # Find the connected components in the graph
+    components = list(nx.connected_components(g))
+
+    # Convert the components to a list of clusters
+    clusters = [list(component) for component in components]
+
+    return clusters
+
+
+def run_blastn(sequences):
+    """
+    run blastn on fasta file
+    :param sequences : dictionary with sequences
+    :return: dictionary with clusters
+    """
+    tmp_dir = tempfile.mkdtemp()
+    fasta_file = F'{tmp_dir}/seqs.fasta'
+    blast_out = F'{tmp_dir}/blastn.out'
+    save_fasta_dict_to_file(sequences, fasta_file)
+    # make blast database
+    cmd = F"makeblastdb -in {tmp_dir}/seqs.fasta -dbtype nucl"
+    subprocess.check_call(cmd, shell=True)
+    # run blastn
+
+    outfmt = "'6 qseqid sseqid pident length evalue bitscore qlen slen'"
+
+    cmd = (F"blastn -task blastn -query {fasta_file} -db {fasta_file} -outfmt {outfmt}"
+           F" -out {blast_out} -num_threads 30 -evalue 1e-20 -perc_identity 75"
+           F" -word_size 9 -max_target_seqs 1000000 -dust no "
+           F" -gapextend 1 -gapopen 2 -reward 1 -penalty -1")
+
+    subprocess.check_call(cmd, shell=True)
+    # read pairs to list, exclude self hits and duplicates
+    pairs = set()
+    with open(blast_out, 'r') as f:
+        for line in f:
+            qseqid, sseqid, pident, length, evalue, bitscore, qlen, slen = line.split()
+            if qseqid != sseqid:
+                # overlap should be at least 50% over the shorter sequence
+                if int(length) / min([int(qlen), int(slen)]) > 0.8:
+                    pairs.add(tuple(sorted([qseqid, sseqid])))
+    # add self hits separately, so they are in the graph later
+    for seq_id in sequences:
+        pairs.add((seq_id, seq_id))
+
+    rmtree(tmp_dir)
+    return pairs
+
+
+def find_clusters_by_blast_connected_component(consensus_representative):
+    """
+    find clusters by blastn, return dictionary with clusters
+    cluaste are connected components in graph
+    :param consensus_representative:
+    :return: clusters
+    """
+    pairs = run_blastn(consensus_representative)
+    # graph = make_graph(pairs)
+    # components = find_connected_components(graph)
+    components = get_connected_component_clusters(pairs)
+    clusters = {}
+    for vertices in components:
+        v_representative = sorted(vertices)[0]
+        for v in vertices:
+            clusters[v] = v_representative
+
+    return clusters
+
+
+def get_cluster_size(fin, clusters):
+    """
+    get cluster size
+    :param fin: input gff
+    :param clusters: clusters dictionary id:cluster_id
+    :return: cluster size as total length of intervals
+    """
+    cluster_size = {}
+    with open(fin, 'r') as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+            gff3_feature = Gff3Feature(line)
+            cluster_id = clusters[gff3_feature.attributes_dict['ID']]
+            if cluster_id not in cluster_size:
+                cluster_size[cluster_id] = 0
+            cluster_size[cluster_id] += gff3_feature.end - gff3_feature.start
+    return cluster_size
+
+
+def add_cluster_info_to_gff3(fin, fout, clusters):
+    """
+    add cluster info to gff3 file
+    :param fin: input gff
+    :param fout: output gff
+    :param clusters: clusters dictionary id:cluster_id
+    :return: consensus and consensus dimers of representative sequences
+    """
+    consensus_clusters = {}
+    consensus_clusters_dimers = {}
+    with open(fin, 'r') as f1, open(fout, 'w') as f2:
+        for line in f1:
+            if line.startswith("#"):
+                f2.write(line)
+                continue
+            gff3_feature = Gff3Feature(line)
+            cluster_id = clusters[gff3_feature.attributes_dict['ID']]
+            gff3_feature.attributes_dict['Cluster_ID'] = cluster_id
+            gff3_feature.attributes_dict['Name'] = cluster_id
+            unique_id = cluster_id + "_" + gff3_feature.attributes_dict['ID']
+            gff3_feature.attributes_dict['ID'] = unique_id
+            # add consensus sequence to dictionary of dictionaries
+            consensus = gff3_feature.attributes_dict['Consensus_sequence']
+            if cluster_id not in consensus_clusters:
+                consensus_clusters[cluster_id] = {}
+                consensus_clusters_dimers[cluster_id] = {}
+            consensus_clusters[cluster_id][unique_id] = consensus
+            consensus_clusters_dimers[cluster_id][unique_id] = consensus * 2
+
+            f2.write(gff3_feature.print_line())
+    return consensus_clusters, consensus_clusters_dimers
+
+
+def merge_overlapping_gff3_intervals(gff3_file, gff3_out_file):
+    """
+    merge overlapping intervals in gff3 file
+    merge only if they have same Cluster_ID
+    :param gff3_file: path to gff3 file
+    :param gff3_out_file: path to output gff3 file
+    :return: nothing
+    """
+    # read gff3 file, split to lists by cluster ID and seqname
+    gff3_dict = {}
+    with open(gff3_file, 'r') as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+            gff3_feature = Gff3Feature(line)
+            cluster_id = gff3_feature.attributes_dict['Cluster_ID']
+            seqname = gff3_feature.seqid
+            seq_cluster_id = (seqname, cluster_id)
+            if seq_cluster_id not in gff3_dict:
+                gff3_dict[seq_cluster_id] = []
+            gff3_dict[seq_cluster_id].append(gff3_feature)
+    with open(gff3_out_file, 'w') as f:
+        f.write("##gff-version 3\n")
+        for i in gff3_dict:
+            intervals = []
+            for j in gff3_dict[i]:
+                intervals.append((j.start, j.end))
+            intervals = merge_intervals(intervals)
+            for start, end in intervals:
+                gff_line = (F'{i[0]}\tTideCluster\trepeat_region'
+                            F'\t{start}\t{end}\t{1}\t.\t.\tName={i[1]}\n')
+
+                f.write(gff_line)
+
+
+def merge_intervals(intervals):
+    """
+    :param intervals:list of (start, end) tuples
+    :return:
+    list of merged (start, end) tuples
+    """
+    # Sort intervals by start point
+    intervals.sort()
+    # Initialize the merged intervals list with the first interval
+    merged = [intervals[0]]
+    for start, end in intervals[1:]:
+        # Check if the current interval overlaps with the last merged interval
+        if start <= merged[-1][1] + 1:
+            # Merge the intervals by extending the end point of the last interval
+            merged[-1] = (merged[-1][0], max(merged[-1][1], end))
+        else:
+            # Add the current interval to the list of merged intervals
+            merged.append((start, end))
+    return merged
+
+
+def filter_gff_by_size(gff3_file, min_size=10000):
+    """
+    Filter gff3 file by size of intervals
+    :param gff3_file:
+    :param min_size:
+    :return: filtered gff3 file path
+    """
+    gff_out = tempfile.NamedTemporaryFile(delete=False).name
+    with open(gff3_file, 'r') as f1, open(gff_out, 'w') as f2:
+        for line in f1:
+            if line.startswith("#"):
+                f2.write(line)
+                continue
+            gff3_feature = Gff3Feature(line)
+            if gff3_feature.end - gff3_feature.start > min_size:
+                f2.write(line)
+    return gff_out
+
+
+def save_consensus_files(consensus_dir, cons_cls, cons_cls_dimer):
+    """
+    Save consensus sequences to fasta files
+    :param consensus_dir: directory to save consensus sequences
+    :param cons_cls: consensus sequences for each cluster
+    :param cons_cls_dimer: consensus sequences for each cluster, dimers
+    """
+    if not os.path.exists(consensus_dir):
+        os.makedirs(consensus_dir)
+    for cluster_id in cons_cls:
+        f = os.path.join(consensus_dir, cluster_id + ".fasta")
+        save_fasta_dict_to_file(cons_cls[cluster_id], f)
+        f = os.path.join(consensus_dir, cluster_id + "_dimers.fasta")
+        save_fasta_dict_to_file(cons_cls_dimer[cluster_id], f)
